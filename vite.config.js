8573f5309266f295321f2e35e10b95486a866acb
AdhemarMolon <162194104+AdhemarMolon@users.noreply.github.com>
2025-08-07 12:14:17 -0300

    feat: finally add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.ts - A VERSÃO CORRETA E FUNCIONAL
+// vite.config.ts - AGORA SIM A VERSÃO CORRETA E FUNCIONAL
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 export default defineConfig({
+    // ESSA É A LINHA QUE FALTAVA
+    base: "/portfolio/",
     plugins: [react()],
     resolve: {
         alias: {
