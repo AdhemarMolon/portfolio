a1a616410504b21b0ab4e09b6f65fc7fd99a4f5a
AdhemarMolon <162194104+AdhemarMolon@users.noreply.github.com>
2025-09-21 14:07:41 -0300

    Removendo nome da NavBar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -31,10 +31,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
-            <div className="w-10 h-10 hero-gradient rounded-lg flex items-center justify-center">
-              <span className="text-lg font-bold text-primary-foreground">A</span>
-            </div>
-            <span className="text-xl font-bold text-foreground">Adhemar</span>
+        
           </Link>
 
           {/* Desktop Navigation */}
